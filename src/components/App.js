4715db0f3ed6bda129c8b6eb829b5a5bb959dd7d
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2024-03-13 16:57:54 +0000

    added solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,58 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTodo = () => {
+    // console.log(inputValue);
+    let found = false;
+    for (let i = 0; i < tasks.length; i++) {
+      let task = tasks[i];
+      if (task === inputValue) {
+        alert("Already added the task")
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      setTasks([...tasks, inputValue]);
+      setInputValue('');
+    }
+  }
+
+  const removeTask = (taskTobeRemoved) => {
+    const remainingTasks = tasks.filter(task => task !== taskTobeRemoved);
+    setTasks(remainingTasks);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ margin: "20px" }}>
+      <h2>To-Do List</h2>
+      <div>
+        <input
+          placeholder="Enter Todo"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul className="tasks-list">
+        {
+          tasks.map(task => {
+            return (
+              <li className="task">
+                <span>{task}</span>
+                <button onClick={() => removeTask(task)}>Delete</button>
+              </li>
+            );
+          })
+        }
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
